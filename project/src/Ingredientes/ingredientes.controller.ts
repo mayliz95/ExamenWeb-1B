ef7b0d1205decbe781694d0c8e00a73719fe5a09
mayliz95 <mayra.rosero@epn.edu.ec>
2018-05-30 15:34:55 -0500

    Controller de ingredientes

--- project/src/Ingredientes/ingredientes.controller.ts
+++ project/src/Ingredientes/ingredientes.controller.ts
@@ -1,6 +1,25 @@
-import {Controller} from "@nestjs/common";
+import {Controller, Get, Post, Put} from "@nestjs/common";
 
 @Controller('Ingredientes')
 export class IngredientesController {
 
+    @Get()
+    listarTodos() {
+        return 'Listar Todos';
+    }
+
+    @Post()
+    crearIngredientes() {
+        return 'crear ingrediente';
+    }
+
+    @Get('/:id')
+    obtenerUno() {
+        return 'Obtener uno';
+    }
+
+    @Put('/:id')
+    editarUno() {
+        return 'Editar uno';
+    }
 }
