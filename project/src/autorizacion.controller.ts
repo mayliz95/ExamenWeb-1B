c69794f55388abd9d4b3af27bdd91450816443da
mayliz95 <mayra.rosero@epn.edu.ec>
2018-06-01 12:52:49 -0500

    metodos del controlador autorizacion.. falta parte de las cookies

--- project/src/autorizacion.controller.ts
+++ project/src/autorizacion.controller.ts
@@ -1,13 +1,42 @@
-import {Body, Controller, Post} from "@nestjs/common";
+import {Body, Controller, Get, Post, Req, Res} from "@nestjs/common";
 
 @Controller('Autorizacion')
 export class AutorizacionController {
 
     @Post('IniciarSesion')
     iniciarSesion(
-        @Body() bodyParams
+        @Body() bodyParams,
+        @Req() request,
+        @Res() response
     ) {
-        const usuario = bodyParams.usuario;
-        const password = bodyParams.password;
+        //response.cookie('token', 'adrianeguez');
+        if (bodyParams.usuario === 'adrianeguez' && bodyParams.password === '12345678910') {
+
+            return response.send({
+                mensaje: 'OK'
+            })
+        }
     }
+
+    @Get('anadirCookie')
+    anadirCookie(
+        @Res() response,
+        @Req() request,
+    ) {
+        response.cookie('token', 'adrianeguez');
+        return response.send();
+    }
+
+    @Post('cerrarSesion')
+    cerrarSesion(
+        @Body() bodyParams,
+        @Req() request,
+        @Res() response
+    ) {
+        //response.cookie('token', 'adrianeguez');
+        return response.send({
+            mensaje: 'Usted salio del sistema'
+        })
+    }
+
 }
